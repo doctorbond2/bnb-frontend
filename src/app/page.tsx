fd994fdf077c291c14018374854c358793cae07c
doctorbond2 <michael.rostlund@hotmail.com>
2024-11-04 12:12:22 +0100

    Add dynamic export to Home component and clean up updatePropertyReducer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ const getPropertyList = async () => {
   });
   return response;
 };
+export const dynamic = 'force-dynamic';
 export default async function Home() {
   const response = await getPropertyList();
 
